6805e1e164b3517de5c2627d92a95788c5bc47ca
syedm1 <31075630+syedm1@users.noreply.github.com>
2021-09-28 23:47:13 +1000

    BOILER 3

--- next.config.js
+++ next.config.js
@@ -1,21 +1,56 @@
-// next.config.js
-const withPlugins = require('next-compose-plugins');
-const offline = require('next-offline');
+const {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} = require('next/constants')
 
-const nextConfig = {
-  target: process.env.NODE_ENV !== 'production' ? 'server' : 'serverless',
-  dontAutoRegisterSw: true,
-  generateSw: false,
-  devSwSrc: './public/sw.js',
-  workboxOpts: {
-    swSrc: './public/sw.js',
-    swDest: './public/service-worker.js',
-  },
-  // Exposes Server ENV Vars To Client Using Webpack
-  env: {
-    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-  },
-};
+const getBuildConfig = () => {
+  const path = require('path')
+  const postcssPresetEnv = require('postcss-preset-env')
+  const postcssPresetEnvOptions = {
+    features: {
+      'custom-media-queries': true,
+      'custom-selectors': true,
+    },
+  }
 
-module.exports = withPlugins([[offline]], nextConfig);
+  const cssOptions = {
+    postcssLoaderOptions: {
+      plugins: [postcssPresetEnv(postcssPresetEnvOptions)],
+    },
+    sassOptions: {
+      includePaths: [path.join(process.cwd(), 'src', 'common', 'css')],
+    },
+  }
+
+  const nextConfig = {
+    ...cssOptions,
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        include: path.join(process.cwd(), 'src', 'components', 'icon', 'icons'),
+        use: [
+          'svg-sprite-loader',
+          {
+            loader: 'svgo-loader',
+            options: {
+              plugins: [
+                { removeAttrs: { attrs: '(fill)' } },
+                { removeTitle: true },
+                { cleanupIDs: true },
+                { removeStyleElement: true },
+              ],
+            },
+          },
+        ],
+      })
+      return config
+    },
+  }
+  return nextConfig
+}
+
+module.exports = (phase) => {
+  const shouldAddBuildConfig =
+    phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD
+  return shouldAddBuildConfig ? getBuildConfig() : {}
+}
